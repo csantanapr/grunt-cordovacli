bc645b448eac66a2645d34b3c2eb0b7fc4da3683
Carlos Santana <csantana23@gmail.com>
2013-08-26 15:26:28 -0400

    updated auth info

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,25 +73,25 @@ module.exports = function (grunt) {
                     command: 'plugin',
                     path: 'myHybridAppFolder',
                     action: 'add',
-                    plugins: [  
-                                'vibration',
-                                'camera',
-                                'device-orientation',
-                                'network-information',
-                                'media',
-                                'device',
-                                'contacts',
-                                'battery-status',
-                                'splashscreen',
-                                'media-capture',
-                                'inappbrowser',
-                                'globalization',
-                                'geolocation',
-                                'file',
-                                'dialogs',
-                                'device-motion',
-                                'console'
-                             ]
+                    plugins: [
+                        'vibration',
+                        'camera',
+                        'device-orientation',
+                        'network-information',
+                        'media',
+                        'device',
+                        'contacts',
+                        'battery-status',
+                        'splashscreen',
+                        'media-capture',
+                        'inappbrowser',
+                        'globalization',
+                        'geolocation',
+                        'file',
+                        'dialogs',
+                        'device-motion',
+                        'console'
+                    ]
                 }
             }
         },
@@ -105,16 +105,16 @@ module.exports = function (grunt) {
 
     // Actually load this plugin's task(s).
     grunt.loadTasks('tasks');
-    
+
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    
+
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
     grunt.registerTask('test', ['clean', 'cordovatest', 'nodeunit']);
-    
+
     // By default, lint and run all tests.
     grunt.registerTask('default', ['jshint', 'test']);
 
